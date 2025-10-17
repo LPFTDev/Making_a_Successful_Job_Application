1d344bde3329af2a77a571621bad319253d6a856
LPFTDev <156452759+LPFTDev@users.noreply.github.com>
2025-10-17 09:47:59 +0100

    Making a Successful Job Application

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,6 +2,102 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6hfRwTvehaf":
+        Script1();
+        break;
+      case "5yqFDyfgyiL":
+        Script2();
+        break;
+      case "5k7KHkCr35z":
+        Script3();
+        break;
+      case "69bILc9jpYr":
+        Script4();
+        break;
+      case "62ZxwXC4ZXk":
+        Script5();
+        break;
+      case "5c14o8ATaKO":
+        Script6();
+        break;
+      case "6h4Nug9pmDU":
+        Script7();
+        break;
+      case "6jrNIApfchG":
+        Script8();
+        break;
+      case "6YVUcGgYkpL":
+        Script9();
+        break;
+      case "6MIOrvnrsBG":
+        Script10();
+        break;
+      case "6qfW2OVGVUx":
+        Script11();
+        break;
+      case "5bNSS9Xde1i":
+        Script12();
+        break;
+      case "5pBEZkT7TGc":
+        Script13();
+        break;
+      case "5mEhLoyqhQh":
+        Script14();
+        break;
+      case "6VRrb6lZd0F":
+        Script15();
+        break;
+      case "6KaaPxn4Ws0":
+        Script16();
+        break;
+      case "6GdsHTx35UQ":
+        Script17();
+        break;
+      case "67G32auiP1T":
+        Script18();
+        break;
+      case "68UAaYGXeEo":
+        Script19();
+        break;
+      case "5woAewWmj7e":
+        Script20();
+        break;
+      case "5vGpPxJPd7M":
+        Script21();
+        break;
+      case "5gBiGFkS0L5":
+        Script22();
+        break;
+      case "5nlpSM3uduL":
+        Script23();
+        break;
+      case "6qhPttJrY8v":
+        Script24();
+        break;
+      case "5evNaNq7m11":
+        Script25();
+        break;
+      case "5fu9ldMlDsj":
+        Script26();
+        break;
+      case "6iaNo0ENT33":
+        Script27();
+        break;
+      case "6DY9ZZ7u4Lf":
+        Script28();
+        break;
+      case "6Eb0r8kW3bs":
+        Script29();
+        break;
+      case "5oAiZ2pnh3y":
+        Script30();
+        break;
+      case "5wjrhcPujEX":
+        Script31();
+        break;
+      case "6NDdaOGuiFq":
+        Script32();
+        break;
   }
 }
 
@@ -20,4 +116,412 @@ var showPointer = player.showPointer;
 var hidePointer = player.hidePointer;
 var slideWidth = player.slideWidth;
 var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('6e75cOw9CME');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '66OMvFQ5yKL';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6e75cOw9CME');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '66OMvFQ5yKL_reverse';
+const growAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('6e75cOw9CME');
+const duration = 1000;
+const easing = 'ease-in-out';
+const id = '5izWfa8FkyC';
+const shrinkAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 - shrinkAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script4 = function()
+{
+  const target = object('6e75cOw9CME');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '66OMvFQ5yKL';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script5 = function()
+{
+  const target = object('6FOSawrBg9N');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5l06HZfHnFf';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script6 = function()
+{
+  const target = object('6FOSawrBg9N');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5l06HZfHnFf_reverse';
+const growAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script7 = function()
+{
+  const target = object('6FOSawrBg9N');
+const duration = 1000;
+const easing = 'ease-in-out';
+const id = '5shPMi4EmNL';
+const shrinkAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 - shrinkAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script8 = function()
+{
+  const target = object('6FOSawrBg9N');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5l06HZfHnFf';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script9 = function()
+{
+  const target = object('6WrCYcn74RX');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '66OMvFQ5yKL';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script10 = function()
+{
+  const target = object('6WrCYcn74RX');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '66OMvFQ5yKL_reverse';
+const growAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script11 = function()
+{
+  const target = object('6WrCYcn74RX');
+const duration = 1000;
+const easing = 'ease-in-out';
+const id = '5izWfa8FkyC';
+const shrinkAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 - shrinkAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script12 = function()
+{
+  const target = object('6WrCYcn74RX');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '66OMvFQ5yKL';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script13 = function()
+{
+  const target = object('6UUMEmNiXcM');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5l06HZfHnFf';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script14 = function()
+{
+  const target = object('6UUMEmNiXcM');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5l06HZfHnFf_reverse';
+const growAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script15 = function()
+{
+  const target = object('6UUMEmNiXcM');
+const duration = 1000;
+const easing = 'ease-in-out';
+const id = '5shPMi4EmNL';
+const shrinkAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 - shrinkAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script16 = function()
+{
+  const target = object('6UUMEmNiXcM');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5l06HZfHnFf';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script17 = function()
+{
+  const target = object('61wjqMYcluQ');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '66OMvFQ5yKL';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script18 = function()
+{
+  const target = object('61wjqMYcluQ');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '66OMvFQ5yKL_reverse';
+const growAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script19 = function()
+{
+  const target = object('61wjqMYcluQ');
+const duration = 1000;
+const easing = 'ease-in-out';
+const id = '5izWfa8FkyC';
+const shrinkAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 - shrinkAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script20 = function()
+{
+  const target = object('61wjqMYcluQ');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '66OMvFQ5yKL';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script21 = function()
+{
+  const target = object('6WqWHXhFUAs');
+const duration = 1000;
+const easing = 'ease-in-out';
+const id = '5l06HZfHnFf';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script22 = function()
+{
+  const target = object('6WqWHXhFUAs');
+const duration = 1000;
+const easing = 'ease-in-out';
+const id = '5l06HZfHnFf_reverse';
+const growAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script23 = function()
+{
+  const target = object('6WqWHXhFUAs');
+const duration = 1000;
+const easing = 'ease-in-out';
+const id = '5shPMi4EmNL';
+const shrinkAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 - shrinkAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script24 = function()
+{
+  const target = object('6WqWHXhFUAs');
+const duration = 1000;
+const easing = 'ease-in-out';
+const id = '5l06HZfHnFf';
+const growAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
 };
--- story_content/user.js
+++ story_content/user.js
@@ -13,4 +13,324 @@ var showPointer = player.showPointer;
 var hidePointer = player.hidePointer;
 var slideWidth = player.slideWidth;
 var slideHeight = player.slideHeight;
+window.Script25 = function()
+{
+  let player = GetPlayer(); 
+
+//load the arrays
+let RecFocS = JSON.parse(sessionStorage.getItem("RecFocS")); 
+let CanDoS = JSON.parse(sessionStorage.getItem("CanDoS")); 
+let PositiveFutureS = JSON.parse(sessionStorage.getItem("PositiveFutureS")); 
+let NotRecFocS = JSON.parse(sessionStorage.getItem("NotRecFocS")); 
+let CantDoS = JSON.parse(sessionStorage.getItem("CantDoS")); 
+let NegativeFutureS = JSON.parse(sessionStorage.getItem("NegativeFutureS")); 
+
+console.log('BEGIN INCORRECT'); 
+
+//pick a number again 
+const CorrectNo = Math.floor(Math.random() * RecFocS.length); 
+
+
+// set the variables
+let RecFoc = RecFocS[CorrectNo]; 
+let CanDo = CanDoS[CorrectNo]; 
+let PositiveFuture = PositiveFutureS[CorrectNo]; 
+let NotRecFoc = NotRecFocS[CorrectNo]; 
+let CantDo = CantDoS[CorrectNo]; 
+let NegativeFuture = NegativeFutureS[CorrectNo]; 
+
+
+player.SetVar("RecFoc",RecFoc); 
+player.SetVar("CanDo",CanDo); 
+player.SetVar("PositiveFuture",PositiveFuture); 
+player.SetVar("NotRecFoc",NotRecFoc); 
+player.SetVar("CantDo",CantDo); 
+player.SetVar("NegativeFuture",NegativeFuture); 
+
+
+
+console.log('END INCORRECT'); 
+console.log('RecFocS: ' + RecFocS); 
+
+
+//save the new arrays
+sessionStorage.setItem("RecFocS", JSON.stringify(RecFocS)); 
+sessionStorage.setItem("CanDoS", JSON.stringify(CanDoS)); 
+sessionStorage.setItem("PositiveFutureS", JSON.stringify(PositiveFutureS)); 
+sessionStorage.setItem("NotRecFocS", JSON.stringify(NotRecFocS)); 
+sessionStorage.setItem("CantDoS", JSON.stringify(CantDoS)); 
+sessionStorage.setItem("NegativeFutureS", JSON.stringify(NegativeFutureS)); 
+}
+
+window.Script26 = function()
+{
+  let player = GetPlayer(); 
+
+//load the arrays
+let RecFocS = JSON.parse(sessionStorage.getItem("RecFocS")); 
+let CanDoS = JSON.parse(sessionStorage.getItem("CanDoS")); 
+let PositiveFutureS = JSON.parse(sessionStorage.getItem("PositiveFutureS")); 
+let NotRecFocS = JSON.parse(sessionStorage.getItem("NotRecFocS")); 
+let CantDoS = JSON.parse(sessionStorage.getItem("CantDoS")); 
+let NegativeFutureS = JSON.parse(sessionStorage.getItem("NegativeFutureS")); 
+
+console.log('BEGIN CORRECT'); 
+
+
+//look up the index of the current subject in the arrays
+var CurrSub = player.GetVar("RecFoc"); 
+let SpliceIndex = RecFocS.indexOf(CurrSub); 
+
+
+//splice the current items out of the arrays
+RecFocS.splice(SpliceIndex, 1); 
+CanDoS.splice(SpliceIndex, 1); 
+PositiveFutureS.splice(SpliceIndex, 1); 
+NotRecFocS.splice(SpliceIndex, 1); 
+CantDoS.splice(SpliceIndex, 1); 
+NegativeFutureS.splice(SpliceIndex, 1); 
+
+
+//pick a number again, based on the new length of the arrays
+const CorrectNo = Math.floor(Math.random() * RecFocS.length)
+
+
+// set the variables
+let RecFoc = RecFocS[CorrectNo]; 
+let CanDo = CanDoS[CorrectNo]; 
+let PositiveFuture = PositiveFutureS[CorrectNo]; 
+let NotRecFoc = NotRecFocS[CorrectNo]; 
+let CantDo = CantDoS[CorrectNo]; 
+let NegativeFuture = NegativeFutureS[CorrectNo]; 
+
+
+player.SetVar("RecFoc",RecFoc); 
+player.SetVar("CanDo",CanDo); 
+player.SetVar("PositiveFuture",PositiveFuture); 
+player.SetVar("NotRecFoc",NotRecFoc); 
+player.SetVar("CantDo",CantDo); 
+player.SetVar("NegativeFuture",NegativeFuture); 
+
+
+console.log('END CORRECT'); 
+console.log('RecFocS: ' + RecFocS); 
+
+
+//save the new arrays
+sessionStorage.setItem("RecFocS", JSON.stringify(RecFocS)); 
+sessionStorage.setItem("CanDoS", JSON.stringify(CanDoS)); 
+sessionStorage.setItem("PositiveFutureS", JSON.stringify(PositiveFutureS)); 
+sessionStorage.setItem("NotRecFocS", JSON.stringify(NotRecFocS)); 
+sessionStorage.setItem("CantDoS", JSON.stringify(CantDoS)); 
+sessionStorage.setItem("NegativeFutureS", JSON.stringify(NegativeFutureS)); 
+}
+
+window.Script27 = function()
+{
+  var player = GetPlayer();
+
+//ADD ANY GLOBAL CSS HERE - I have changed the font to Calibri and set the white-space to allow for line breaks
+var HtmlHead = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><style>body {font-family: Calibri, Arial, sans-serif; white-space: pre;}</style></head><body>";
+  
+var EndHtml = "</body></html>";
+  
+// ENTER YOUR HTML/CONTENT HERE - use strings for tags and player.GetVar for the Storyline variables
+var html = HtmlHead +
+"<h1>My Own Disclosure</h1>"
++ player.GetVar("TextEntry")
+ + EndHtml;
+
+//This specifies the type
+var blob = new Blob(['\ufeff', html], {
+type: 'application/msword'
+    });
+    
+// This specifies the link url
+var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+    
+// SPECIFY THE FILE NAME HERE
+filename = 'My Own Disclosure.doc';
+    
+// This specifies the download link element
+var downloadLink = document.createElement("a");
+
+document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob ){
+        navigator.msSaveOrOpenBlob(blob, filename);
+    }else{
+        // This sets the link to the file
+        downloadLink.href = url;
+        
+        // This sets the file name
+        downloadLink.download = filename;
+        
+        // This triggers the function
+       downloadLink.click();
+    }
+}
+
+window.Script28 = function()
+{
+  let player = GetPlayer(); 
+
+//Array of answers
+
+//RecFoc
+const RecFocS = [
+"I was born with a spine and hip condition. I have had surgeries and regular physiotherapy ", 
+"I experienced a period of ill-health in my past. I have accessed appropriate support and have learnt helpful ways to manage this.", 
+"After a bereavment I have taken the time to learn helpful way to manage it.", 
+"I was diagnosed with a mental health condition as a teenager. Since then I have developed understanding and strategies", 
+"Despite multiple episodes of uncertainty with my health, I have always endevoured to continue on."
+];
+
+//CanDo
+const CanDoS = [
+"which have allowed me to walk short distances unaided, make use of a wheelchair and a specially adapted vehicle. ", 
+"I have maintained my skills and developed new ones during this period.",
+" I am ready to reenter the workforce part-time, ", 
+"I can now recognise the early warning signs and with regular check-ins with my supervisor, ", 
+"I believe this tenacity will serve me well in the workplace."
+];
+
+//PositiveFuture
+const PositiveFutureS = [
+"I hope to bring forward the resillience and commitment I have developed into the workplace.", 
+"To continue my recovery I am now determined to take responsibility for my life and regain meaningful employment. ", 
+"with an aim to increase my hours in a manageable way with an aim for full-time hours.", 
+"I look forward to managing my mental health alongside my job role.", 
+"I hope that in the future I will not have further complications, but know that I can manage it and will return more determined."
+];
+
+//NotRecFoc
+const NotRecFocS = [
+"I'm always at appointments to make my degenerative condition from getting worse.", 
+"I suffered from a period of ill health and became unable to work for some time. ", 
+"My life fell apart after a bereavament.", 
+"I suffer from a long term mental health condition", 
+"I have had multiple, unforseen periods of crisis. Sometimes accompanied by psychosis"
+];
+
+//CantDo
+const CantDoS = [
+"I can't use stairs or walk long distances on my own. If a building doesn't have an elevator I'm stuck on the ground floor.", 
+"I haven’t stuck many jobs out.", 
+"I can't manage working full time yet, ", 
+"My mood may become unpredictable and I won't be able to stay at work. ", 
+"I won't be able to handle high-stress situations. "
+];
+
+//NegativeFuture
+const NegativeFutureS = [
+"I don't have much work experience, and the experience I have was short lived.", 
+"I started self-harming at 16 and fell into the wrong crowd using drugs and started shoplifting to fund my habit. ", 
+"the last time I tried it made me unwell and I had to quit.", 
+"I become unreliable during these periods of my life, and may avoid contact with management.", 
+"I've had lengthy hospital stays in the past when I became unwell."
+];
+
+
+// pick a question and set the variables
+const CorrectNo = Math.floor(Math.random() * RecFocS.length); 
+let RecFoc = RecFocS[CorrectNo]; 
+let CanDo = CanDoS[CorrectNo]; 
+let PositiveFuture = PositiveFutureS[CorrectNo]; 
+let NotRecFoc = NotRecFocS[CorrectNo]; 
+let CantDo = CantDoS[CorrectNo]; 
+let NegativeFuture = NegativeFutureS[CorrectNo]; 
+
+player.SetVar("RecFoc",RecFoc); 
+player.SetVar("CanDo",CanDo); 
+player.SetVar("PositiveFuture",PositiveFuture); 
+player.SetVar("NotRecFoc",NotRecFoc); 
+player.SetVar("CantDo",CantDo); 
+player.SetVar("NegativeFuture",NegativeFuture); 
+
+
+
+// store the arrays
+sessionStorage.setItem("RecFocS", JSON.stringify(RecFocS)); 
+sessionStorage.setItem("CanDoS", JSON.stringify(CanDoS)); 
+sessionStorage.setItem("PositiveFutureS", JSON.stringify(PositiveFutureS)); 
+sessionStorage.setItem("NotRecFocS", JSON.stringify(NotRecFocS)); 
+sessionStorage.setItem("CantDoS", JSON.stringify(CantDoS)); 
+sessionStorage.setItem("NegativeFutureS", JSON.stringify(NegativeFutureS)); 
+
+console.log('END SETUP'); 
+}
+
+window.Script29 = function()
+{
+  
+// Get the Storyline player object
+var player = GetPlayer();
+
+// Simulate clicks by setting variables to true
+player.SetVar("Hotspot9Clicked", true);
+player.SetVar("Hotspot010Clicked", true);
+player.SetVar("Hotspot011Clicked", true);
+
+}
+
+window.Script30 = function()
+{
+  
+// Get the Storyline player
+var player = GetPlayer();
+
+// Get the current text entry value
+var textValue = player.GetVar("TextEntry3");
+
+// Set maximum character limit
+var maxChars = 25;
+
+// Calculate remaining characters (can go negative)
+var remainingChars = maxChars - textValue.length;
+
+// Update CharCount variable with remaining characters
+player.SetVar("CharCount_01", remainingChars);
+
+}
+
+window.Script31 = function()
+{
+  
+// Get the Storyline player
+var player = GetPlayer();
+
+// Get the current text entry value
+var textValue = player.GetVar("TextEntry3");
+
+// Set maximum character limit
+var maxChars = 25;
+
+// Calculate remaining characters (can go negative)
+var remainingChars = maxChars - textValue.length;
+
+// Update CharCount variable with remaining characters
+player.SetVar("CharCount_02", remainingChars);
+
+}
+
+window.Script32 = function()
+{
+  
+// Get the Storyline player
+var player = GetPlayer();
+
+// Get the current text entry value
+var textValue = player.GetVar("TextEntry3");
+
+// Set maximum character limit
+var maxChars = 25;
+
+// Calculate remaining characters (can go negative)
+var remainingChars = maxChars - textValue.length;
+
+// Update CharCount variable with remaining characters
+player.SetVar("CharCount_03", remainingChars);
+
+}
+
 };
